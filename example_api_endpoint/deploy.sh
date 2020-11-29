9937431445e99a52bce493aef7b8aaf6ba89d3d8
Sadik Moazzem <sadik-moazzem@outlook.com>
2020-11-29 19:15:23 +0000

    fixed deploy script

--- example_api_endpoint/deploy.sh
+++ example_api_endpoint/deploy.sh
@@ -1,13 +1,13 @@
-echo --------Deploying GET MASJIDS /get-masjids
+echo --------Deploying EXAMPLE ENDPOINT /example-entpoint
 python3 -m pip install --target ./package -r ./requirements.txt -q
 cd package
 cd ..
 mkdir lambda
-cp get_masjids/lambda_function.py lambda/
+cp lambda_function.py lambda/
 cp -r package/. lambda/
 cd lambda
 zip -q -r my-deployment-package.zip *
-aws lambda update-function-code --function-name mylocalmasjid-getMasjids --zip-file fileb://./my-deployment-package.zip
+aws lambda update-function-code --function-name [LAMBA_NAME] --zip-file fileb://./my-deployment-package.zip
 rm -r my-deployment-package.zip
 cd ../
 rm -r package
